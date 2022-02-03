2a8029901eba8d5eb328912b9d0040853acb1928
Kristof Danko <krysstof@icloud.com>
2022-02-03 01:46:33 +0100

    Test

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p
+          className="App-quest">
+          Ako sa ma?
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
