9789eecd677cb8f6b5962c7d5cc715726d3b2ad9
Kristof Danko <krysstof@icloud.com>
2022-05-08 21:52:33 +0200

    Add React Router

---
+++ src/Home.tsx
@@ -0,0 +1,8 @@
+function Home(){
+    return (<><button type="button">Characters</button>
+            <button type="button">Episodes</button>
+            </>)
+  };
+  
+  export default Home;
+  
